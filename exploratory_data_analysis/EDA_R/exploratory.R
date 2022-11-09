0ff5734c1e18953bee725d8431cfa6b6f000fecc
lige-on-git <tanlige815@outlook.com>
2022-11-09 13:38:29 +1100

    remove all inf values from data

--- exploratory_data_analysis/EDA_R/exploratory.R
+++ exploratory_data_analysis/EDA_R/exploratory.R
@@ -52,3 +52,80 @@ indices <- which(non.members[,golflinkno] %in% example_golfers)
 unique(non.members[indices], by=c("golferid", "golflinkno"))[, .N, by=golflinkno][N>1]
 non.members[golflinkno==5021020]
 non.members[golflinkno==9011939919]  # only these two, a <golflinkno> has multiple <golferid>
+
+
+
+## _____________________________________________________________________________________________
+## Fit the aggregated handicap table and response
+
+responses <- fread(isactive_response_path); nrow(responses)  # less rows as 2022 not included
+golfers.agg <- fread(handicap_aggregated_1_path); nrow(golfers.agg)
+
+responses[is_active==TRUE & year>2013]
+responses[is_active==FALSE  & year>2013]
+
+# - left join golfers.agg to responses so each (golferid, year) pair is associated with an response
+full.xy.table <- golfers.agg[on=c('golferid', 'year'), responses]; nrow(full.xy.table)
+
+
+# - is.na() can apply to a vector or a list (so work for data.table); is.finite() can only apply to a vector/column
+
+full.xy.table[is.infinite(newhandicap_change_ratio)==TRUE]  # is.infinite() works for a single vector/column
+full.xy.table[, is.infinite(newhandicap_change_ratio)]
+
+full.xy.table[, lapply(.SD, FUN = is.infinite)]  # apply to all columns - return a data.table
+is.na(full.xy.table)  # return a Boolean matrix if call is.na() outside (is.finite() and is.infinite() NOT WORK for a list/data.table)
+
+
+# - check percentage of NA values in each column
+(colSums(is.na(full.xy.table)) / nrow(full.xy.table)) * 100  # calculate column sum/mean without grouping-by
+                                                             # using colSums(); similarly there's colMeans()
+
+full.xy.table[, lapply(.SD, FUN = sum)]  # sum() and mean() also only apply to a single vector/column,
+                                         # so use lapply to calculate sum/mean of each column without grouping-by
+na.nums <- function(col){
+  sum(is.na(col))
+}
+full.xy.table[, lapply(.SD, FUN=na.nums)]  # use lapply to trick to calculate number of NA values in each column
+
+
+# - check how many infinite numbers
+inf.nums <- function(col){
+  sum(is.infinite(col))  # unlike is.na(), can't call is.infinite(.SD) because it doesn't work for R list/data.table
+}                        # so wrap it in a function and use <lapply> to apply <on each column> of a list/data.table
+full.xy.table[, lapply(.SD, inf.nums)]
+
+
+# - remove 4 columns with too many NA's
+full.xy.table[, ":=" (average_playinghandicap=NULL, sd_playinghandicap=NULL,
+                      average_sloperating=NULL, sd_sloperating=NULL)]
+
+
+# - change all Inf values to NA, and then remove all rows containing NA 
+
+for(col.name in names(full.xy.table)){
+    
+  if (full.xy.table[, inf.nums(get(col.name))] > 0){  # select using get(string)
+    full.xy.table[is.infinite(get(col.name)), (col.name) := NA]  # assign using (string)
+  }
+}
+
+full.xy.table <- na.omit(full.xy.table)
+
+
+
+## _____________________________________________________________________________________________
+## train some models
+
+# - split training and testing data
+sample <- sample(c(TRUE, FALSE), nrow(full.xy.table), replace=TRUE, prob=c(0.7,0.3))
+train.xy  <- full.xy.table[sample, ]; nrow(train.xy)
+test.xy   <- full.xy.table[!sample, ]; nrow(test.xy)
+
+# - fit model
+names(test.xy)
+train.bin <- glm(as.factor(is_active) ~ . -golferid -year, 
+                 family=binomial, data=test.xy, na.action=na.exclude)
+
+summary(train.bin)
+
